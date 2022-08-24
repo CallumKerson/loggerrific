0ea7b4aa58736fb5a66c8f6c5b0168eb120ef51a
CallumKerrEdwards <github@callumkerredwards.co.uk>
2022-08-24 10:42:20 +0100

    Adding documentation

---
+++ doc.go
@@ -0,0 +1,7 @@
+/*
+Package Package loggerrific is a logging interface for abstracting the choice of logging framework
+for an application.
+
+For a full guide visit https://github.com/CallumKerrEdwards/loggerrific
+*/
+package loggerrific
